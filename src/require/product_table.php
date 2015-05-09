9ad15560faa7442b3e922b0be53f757f579cf666
Allan R. <careitan2000@yahoo.com>
2015-05-08 20:16:24 -0700

    Fixing Buttons

--- src/require/product_table.php
+++ src/require/product_table.php
@@ -19,14 +19,14 @@
 		$stmnt->bind_result($id, $category, $name, $length, $rented);
 
 		echo '<div class="products"><table><thead><th>Category</th><th>Name</th><th>Length</th><th>Rental Status</th>
-		<th class="identity" /></thead>';
+		<th class="identity"></th></thead>';
 		echo '<body>';
 
 		while($stmnt->fetch()){
 
-		echo '<tr><td>'.$category.'</td><td>'.$name.'</td><td>'.$length.'</td>';
-		echo '<td><input class="rental" type=button id='.$id.' value="Check In/Out" OnClick="CheckOut(id)">
-		<input class="free" type=button id='.$id.' value="Delete" OnClick="DeleteItem(id)"></td></tr>';
+		echo '<tr><td>'.$category.'</td><td>'.$name.'</td><td>'.$length.'</td><td>'.$rented.'</td>';
+		echo '<td><input class="rental" type=button rental-id='.$id.' value="Check In/Out" OnClick="CheckOut(rental-id)">
+		<input class="free" type=button free-id='.$id.' value="Delete" OnClick="DeleteItem(free-id)"></td></tr>';
 		}
 		echo '</body></table></div>';
 
