39e1a5e5e0a79ece72f090363f54adfc1a601359
Allan R <careitan2000@yahoo.com>
2015-05-10 16:08:29 -0700

    Style Stuff

--- src/require/video_filter.php
+++ src/require/video_filter.php
@@ -2,7 +2,7 @@
 	error_reporting(E_ALL);
 	ini_set('display_errors', 'On');
 ?>
-<form action="apply_filter.php" method=post>
+<form class="filter" action="apply_filter.php" method=post>
 <label>Category:</label><input type=text list="categories" name="catSel">
 <datalist id="categories">
 	<?php
